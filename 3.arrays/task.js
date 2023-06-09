af62be3230a2f36fd8d57654efb96324284f3db7
Rita Komarova <makomarova91@gmail.com>
2023-06-09 14:08:14 +0300

    Третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,29 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	} else {
+		return arr1.every((item, index) => {
+			if (item === arr2[index]) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (Array.isArray(users)) {
+		let result = users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, index, arr) => {
+			acc += item;
+			if (index === arr.length - 1) {
+				return acc / arr.length;
+			} else {
+				return acc;
+			}
+		}, 0);
+		return result;
+	} else {
+		return 0;
+	}
 }
