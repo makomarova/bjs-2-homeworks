5c4d79925a625d1b88f5d93e731ec220178224a9
Rita Komarova <makomarova91@gmail.com>
2023-06-11 17:29:09 +0300

    Четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		for (mark of marks) {
+			this.marks.push(mark);
+		}
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks && this.marks.length > 0) {
+		let sum = 0;
+		for (mark of this.marks) {
+			sum += mark;
+		}
+		return sum / this.marks.length;
+	}
+	return 0;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	if (this.marks) {
+		delete this.marks;
+	}
+	if (this.subject) {
+		delete this.subject;
+	}
+	this.excluded = reason;
 }
